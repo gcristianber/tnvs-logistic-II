1f67b6ad65f3e34e36b0d5212892e1c64e2cd533
Cristianber Gordora <72675662+gcristianber@users.noreply.github.com>
2023-04-21 14:33:49 +0800

    Update Login.php
    Adding a post method for authenticating

--- app/controller/authentication/Login.php
+++ app/controller/authentication/Login.php
@@ -13,21 +13,18 @@ class Login
         $data = [];
 
         $Users = new Accounts;
-        $_POST["username"] = "super_admin@logistic";
-        $_POST["password"] = "l0gistic@!";
         $arr["username"] = $_POST["username"];
         $arr["password"] = $_POST["password"];
 
         $row = $Users->fetch_user($arr);
 
-        if($row && $row->password == $_POST["password"]){
-            $_SESSION["user"] = $row;
-            
-            redirect("general/dashboard");
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if ($row && $row->password == $_POST["password"]) {
+                $_SESSION["user"] = $row;
+
+                redirect("general/dashboard");
+            }
         }
-
-        print_r($row);
-
 
         $this->view("auth/login", $data);
     }
